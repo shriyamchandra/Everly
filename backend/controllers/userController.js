3eee8be8bc02ae64f7bf7d5f56bf01bb35026d27
shriyamchandra <shriyamchandra757@gmail.com>
2024-10-29 22:05:31 +0530

    second commit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import validator from "validator";
-import bcrypt from "bcrypt"
+import bcrypt from 'bcryptjs';
+
 import jwt from 'jsonwebtoken'
 import userModel from "../models/userModel.js";
 
@@ -83,14 +84,14 @@ const registerUser = async (req, res) => {
 // Route for admin login
 const adminLogin = async (req, res) => {
     try {
-        
-        const {email,password} = req.body
+
+        const { email, password } = req.body
 
         if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
-            const token = jwt.sign(email+password,process.env.JWT_SECRET);
-            res.json({success:true,token})
+            const token = jwt.sign(email + password, process.env.JWT_SECRET);
+            res.json({ success: true, token })
         } else {
-            res.json({success:false,message:"Invalid credentials"})
+            res.json({ success: false, message: "Invalid credentials" })
         }
 
     } catch (error) {
